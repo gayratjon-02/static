2495790ae6718c8db5603b24f9b1f95e5a90583d
gayratjon-02 <s-202386005@sillain.ac.kr>
2026-03-09 04:15:48 +0900

    fix(da): import AuthModule to resolve AuthGuard dependency failure

--- src/components/da/da.module.ts
+++ src/components/da/da.module.ts
@@ -3,9 +3,10 @@ import { DaController } from './da.controller';
 import { DaService } from './da.service';
 import { S3Module } from '../s3/s3.module';
 import { DatabaseModule } from '../../database/database.module';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-    imports: [S3Module, DatabaseModule],
+    imports: [S3Module, DatabaseModule, AuthModule],
     controllers: [DaController],
     providers: [DaService],
     exports: [DaService],
