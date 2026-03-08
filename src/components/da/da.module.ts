f8ca9a9b7a1aa058f497c89321fc0654d8437ac9
gayratjon-02 <s-202386005@sillain.ac.kr>
2026-03-09 04:12:50 +0900

    fix(da): import DatabaseModule to resolve DatabaseService DI error

--- src/components/da/da.module.ts
+++ src/components/da/da.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { DaController } from './da.controller';
 import { DaService } from './da.service';
 import { S3Module } from '../s3/s3.module';
+import { DatabaseModule } from '../../database/database.module';
 
 @Module({
-    imports: [S3Module],
+    imports: [S3Module, DatabaseModule],
     controllers: [DaController],
     providers: [DaService],
     exports: [DaService],
